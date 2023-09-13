17568b7d81c438e43460ee50f5678f1badf20921
Ifeanyi Osuji <oindubuisi@gmail.com>
2023-09-13 20:51:23 +0100

    added new logic to some components

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e11273b8852dfd9"; }
+/******/ 		__webpack_require__.h = function() { return "0395cff140f2c78c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
